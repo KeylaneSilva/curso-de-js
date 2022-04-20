d95f3392fa987397635ab6ceb890c3dff048b2e3
Keylane <keylanessilva07@gmail.com>
2022-04-20 12:34:23 -0300

    outras options do toLocalString

--- js/aula06.js
+++ js/aula06.js
@@ -2,6 +2,8 @@ let precoTotal = 9.99
 
 console.log(new Intl.NumberFormat('pt-br').format(precoTotal))
 console.log(`${precoTotal.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'})}`)
+console.log(`${precoTotal.toLocaleString('de-DE', { style: 'currency', currency: 'EUR'})}`)
+console.log(`${precoTotal.toLocaleString('ja-JA', { style: 'currency', currency: 'JPY'})}`)
 
 let lutador = 'Fedor Vladiirovich Emelianenko'
 let nacionalidade = 'Russo'
